1b97f486cc84f0dccf58bcd5a5e489bcdb92d003
Christian Johansen <christian.johansen@mattilsynet.no>
2024-01-16 12:03:19 +0100

    smilefjes.mattilsynet.no er klar

--- tf/app/main.tf
+++ tf/app/main.tf
@@ -23,33 +23,9 @@ module "smilefjes-ui" {
   ingress = "internal-and-cloud-load-balancing"
 
   dedicated_lb = {
-    managed_zone_name = "smilefjes-dns-managed-zone"
-    fqdn = "smilefjes.mattilsynet.io"
-    # Når NS-oppsettet på mattilsynet.no er klart:
-    # managed_zone_name = "smilefjes-mattilsynet-no-dns-zone"
-    # fqdn = "smilefjes.mattilsynet.no"
-  }
-}
-
-# Når smilefjes.mattilsynet.no er oppe, kan hele denne skrotes
-module "smilefjes-lb" {
-  source = "github.com/Mattilsynet/map-tf-cloudrun-shared-lb?ref=v1.5.0"
-  lb_name = "smilefjes-shared-lb"
-  lb_project_id = var.project_id
-  managed_zone_name = "smilefjes-mattilsynet-no-dns-zone"
-  location = "europe-north1"
-  backends = {
-    smilefjes = {
-      fqdn = "smilefjes.mattilsynet.no."
-      separate_certificate = true
-      enable_ipv6 = true
-    }
-  }
-
-  ssl_policy = {
-    min_tls_version = "TLS_1_2"
-    profile = "CUSTOM"
-    custom_features = ["TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"]
+    managed_zone_name = "smilefjes-mattilsynet-no-dns-zone"
+    fqdn = "smilefjes.mattilsynet.no"
+    enable_ipv6 = true
   }
 }
 
--- tf/dns/main.tf
+++ tf/dns/main.tf
@@ -7,3 +7,21 @@ resource "google_dns_managed_zone" "smilefjes_dns_zone" {
     state = "on"
   }
 }
+
+resource "google_dns_record_set" "smilefjes" {
+  name = "${resource.google_dns_managed_zone.smilefjes_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.smilefjes_dns_zone.name
+  type = "A"
+  ttl = 300
+  project = var.project_id
+  rrdatas = ["34.107.226.56"]
+}
+
+resource "google_dns_record_set" "smilefjes-ipv6" {
+  name = "${resource.google_dns_managed_zone.smilefjes_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.smilefjes_dns_zone.name
+  type = "AAAA"
+  ttl = 300
+  project = var.project_id
+  rrdatas = ["2600:1901:0:7940::"]
+}
