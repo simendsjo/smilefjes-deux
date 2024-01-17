7d3960e4c220eb0000f111899545a4951b142073
Christian Johansen <christian.johansen@mattilsynet.no>
2024-01-17 16:28:10 +0100

    Lag en dedikert side for hvert spisested

--- src/smilefjes/pages.clj
+++ src/smilefjes/pages.clj
@@ -1,10 +1,24 @@
 (ns smilefjes.pages)
 
-(defn render-page [_ctx _page]
-  ;; :page.kind/spisested
+(defn layout [& body]
   [:html
    [:body
-    [:div.grid.place-items-center.h-screen
-     [:div.max-w-md
-      [:h1.text-xl "Smilefjes er snart tilbake"]
-      [:p.mt-5 "Den gamle tjenesten måtte dessverre hastenedlegges på grunn av sikkerhetshensyn, men vi jobber iherdig med å få satt opp en ny smilefjesoversikt."]]]]])
+    body]])
+
+(defn render-spisested [spisested]
+  (layout
+   [:div.grid.place-items-center.h-screen
+    [:div.max-w-md
+     [:h1.text-xl "Smilefjes for " (:tilsynsobjekt/navn spisested) " er snart tilbake"]
+     [:p.mt-5 "Den gamle tjenesten måtte dessverre hastenedlegges på grunn av sikkerhetshensyn, men vi jobber iherdig med å få satt opp en ny smilefjesoversikt."]]]))
+
+(defn render-page [_ctx page]
+  (case (:page/kind page)
+    :page.kind/spisested
+    (render-spisested page)
+
+    (layout
+     [:div.grid.place-items-center.h-screen
+      [:div.max-w-md
+       [:h1.text-xl "Smilefjes er snart tilbake"]
+       [:p.mt-5 "Den gamle tjenesten måtte dessverre hastenedlegges på grunn av sikkerhetshensyn, men vi jobber iherdig med å få satt opp en ny smilefjesoversikt."]]])))
