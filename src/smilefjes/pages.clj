1a55b4309bcd93b8d8cd5e21b5ef88e5157d6eb9
Magnar Sveen <magnar.sveen@mattilsynet.no>
2024-01-17 10:56:58 +0100

    Fortell om nedleggingen av den gamle tjenesten for besøkende

--- src/smilefjes/pages.clj
+++ src/smilefjes/pages.clj
@@ -4,4 +4,6 @@
   [:html
    [:body
     [:div.grid.place-items-center.h-screen
-     [:h1.text-xl ":)"]]]])
+     [:div.max-w-md
+      [:h1.text-xl "Smilefjes er snart tilbake"]
+      [:p.mt-5 "Den gamle tjenesten måtte dessverre hastenedlegges på grunn av sikkerhetshensyn, men vi jobber iherdig med å få satt opp en ny smilefjesoversikt."]]]]])
