7ed0a2026ea858dc654f3e8765c97297203c34da
Magnar Sveen <magnar.sveen@mattilsynet.no>
2024-01-18 14:12:31 +0100

    Skill på karakter 2 og 3 i oppsummeringstekst.

--- src/smilefjes/plakaten.clj
+++ src/smilefjes/plakaten.clj
@@ -3,7 +3,8 @@
 (defn oppsummer-smilefjeskarakter [smilefjeskarakter]
   (case smilefjeskarakter
     ("0" "1") "Tilsynet har ikke avdekket regelverksbrudd som krever oppfølging."
-    ("2" "3") "Tilsynet har avdekket regelverksbrudd som krever oppfølging."))
+    "2" "Tilsynet har avdekket regelverksbrudd som krever oppfølging."
+    "3" "Tilsynet har avdekket alvorlig regelverksbrudd."))
 
 (defn beskriv-karakter [smilefjeskarakter]
   (case smilefjeskarakter
