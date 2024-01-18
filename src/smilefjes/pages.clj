955035e02bfe5ad3672b48cb077dacf21ac519ba
Magnar Sveen <magnar.sveen@mattilsynet.no>
2024-01-18 12:50:33 +0100

    Helt plain side for spisesteder

--- src/smilefjes/pages.clj
+++ src/smilefjes/pages.clj
@@ -1,23 +1,31 @@
-(ns smilefjes.pages)
+(ns smilefjes.pages
+  (:require [medley.core :refer [greatest-by]]))
 
 (defn layout [& body]
   [:html
    [:body
     body]])
 
-(defn oops [tittel]
-  [:div.max-w-md.p-5
-   [:h1.text-xl tittel]
-   [:p.mt-5 "Mattilsynets smilefjestjeneste er nede på grunn av teknisk svikt, men vi jobber iherdig med å få på plass en erstatning."]
-   [:p.mt-5 "I mellomtiden kan du finne tilsynsresultater på Digitaliseringsdirektoratets tjeneste "
-    [:a.text-blue-600.underline.hover:no-underline
-     {:href "https://hotell.difi.no/?dataset=mattilsynet/smilefjes/tilsyn"}
-     "hotell.difi.no"] "."]])
-
 (defn render-spisested [spisested]
   (layout
-   [:div.grid.place-items-center.h-screen
-    (oops (str "Smilefjes for " (:tilsynsobjekt/navn spisested) " er snart tilbake"))]))
+   (let [{:keys [linje1 linje2 poststed postnummer]} (:spisested/adresse spisested)
+         siste-besøk (apply greatest-by :tilsynsbesøk/dato (:tilsynsbesøk/_tilsynsobjekt spisested))]
+     [:div.max-w-screen-md.p-5.mx-auto
+      [:h1.text-xl (:spisested/navn spisested)]
+      [:div linje1]
+      [:div linje2]
+      [:div postnummer " " poststed]
+      [:div.bg-gray-100.p-5.mt-5.flex
+       [:div
+        [:h2.text-l.flex-1 "Siste tilsyn " (str (:tilsynsbesøk/dato siste-besøk))]
+        [:p.text-xs.mt-1 (case (:tilsynsbesøk/smilefjeskarakter siste-besøk)
+                           ("0" "1") "Tilsynet har ikke avdekket regelverksbrudd som krever oppfølging."
+                           ("2" "3") "Tilsynet har avdekket regelverksbrudd som krever oppfølging.")]]
+       [:img.w-14.ml-5 {:src (case (:tilsynsbesøk/smilefjeskarakter siste-besøk)
+                               ("0" "1") "/images/smilefjes.svg"
+                               "2" "/images/strekmunn.svg"
+                               "3" "/images/surmunn.svg")}]]
+      [:p.mt-5.text-xs "Mattilsynets smilefjestjeneste er nede på grunn av teknisk svikt, men vi jobber iherdig med å få på plass en erstatning. Nå har du snublet inn i vårt pågående arbeid. Kos med kaos!"]])))
 
 (defn render-page [_ctx page]
   (case (:page/kind page)
@@ -26,4 +34,10 @@
 
     (layout
      [:div.grid.place-items-center.h-screen
-      (oops "Smilefjes er snart tilbake")])))
+      [:div.max-w-md.p-5
+       [:h1.text-xl "Smilefjes er snart tilbake"]
+       [:p.mt-5 "Mattilsynets smilefjestjeneste er nede på grunn av teknisk svikt, men vi jobber iherdig med å få på plass en erstatning."]
+       [:p.mt-5 "I mellomtiden kan du finne tilsynsresultater på Digitaliseringsdirektoratets tjeneste "
+        [:a.text-blue-600.underline.hover:no-underline
+         {:href "https://hotell.difi.no/?dataset=mattilsynet/smilefjes/tilsyn"}
+         "hotell.difi.no"] "."]]])))
