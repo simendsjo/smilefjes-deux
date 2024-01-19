964c12e704559ed942159a44a805002e0782780e
Magnar Sveen <magnar.sveen@mattilsynet.no>
2024-01-19 15:59:35 +0100

    Vis ett stort og fire små tilsynsresultater

--- src/smilefjes/pages.clj
+++ src/smilefjes/pages.clj
@@ -4,25 +4,55 @@
             [smilefjes.plakaten :as plakaten]
             [smilefjes.ui :as ui]))
 
+(defn formater-dato [dato]
+  (str (.getDayOfMonth dato) "."
+       (.getMonthValue dato) "."
+       (.getYear dato)))
+
+(defn siste-tilsynsresultat [siste-besøk karakter]
+  [:div.bg-white.rounded.border.border-furu-500.px-5.py-2.text-center
+   [:h2.text-l.flex-1 "Siste tilsynsresultat:"]
+   [:img.w-36.my-2 {:title (str "Spisestedet har fått " (plakaten/beskriv-karakter karakter) ".")
+                    :src (plakaten/smilefjes-svg-url karakter)}]
+   (formater-dato (:tilsynsbesøk/dato siste-besøk))])
+
+(defn spisested-info [spisested]
+  (let [{:keys [linje1 linje2 poststed postnummer]} (:spisested/adresse spisested)]
+    [:div
+     [:h1.text-3xl (:spisested/navn spisested)]
+     [:div linje1]
+     [:div linje2]
+     [:div postnummer " " poststed]]))
+
+(defn mini-tilsynsresultat [besøk]
+  (let [karakter (:tilsynsbesøk/smilefjeskarakter besøk)]
+    [:div.py-1.text-center.flex.flex-col.items-center
+     [:img.w-8.my-2 {:title (str "Spisestedet har fått " (plakaten/beskriv-karakter karakter) ".")
+                     :src (plakaten/smilefjes-svg-url karakter)}]
+     [:div.text-xs (formater-dato (:tilsynsbesøk/dato besøk))]]))
+
 (defn render-spisested [ctx spisested]
-  (ui/with-layout ctx
-    (ui/header)
-    (let [{:keys [linje1 linje2 poststed postnummer]} (:spisested/adresse spisested)
-          siste-besøk (apply greatest-by :tilsynsbesøk/dato (:tilsynsbesøk/_tilsynsobjekt spisested))
-          karakter (:tilsynsbesøk/smilefjeskarakter siste-besøk)]
+  (let [besøk (->> (:tilsynsbesøk/_tilsynsobjekt spisested)
+                   (sort-by :tilsynsbesøk/dato)
+                   reverse)
+        siste-besøk (first besøk)
+        karakter (:tilsynsbesøk/smilefjeskarakter siste-besøk)]
+    (ui/with-layout ctx
+      (ui/header)
       [:div.bg-lysegrønn
        [:div.max-w-screen-md.mx-auto.p-5
-        [:h1.text-xl (:spisested/navn spisested)]
-        [:div linje1]
-        [:div linje2]
-        [:div postnummer " " poststed]
-        [:div.bg-gray-100.p-5.mt-5.flex
-         [:div
-          [:h2.text-l.flex-1 "Siste tilsyn " (str (:tilsynsbesøk/dato siste-besøk))]
-          [:p.text-xs.mt-1 (plakaten/oppsummer-smilefjeskarakter karakter)]]
-         [:img.w-14.ml-5 {:title (str "Spisestedet har fått " (plakaten/beskriv-karakter karakter) ".")
-                          :src (plakaten/smilefjes-svg-url karakter)}]]
-        [:p.mt-5.text-xs "Mattilsynets smilefjestjeneste er nede på grunn av teknisk svikt, men vi jobber iherdig med å få på plass en erstatning. Nå har du snublet inn i vårt pågående arbeid. Kos med kaos!"]]])))
+        [:div.flex
+         [:div.flex-1
+          (spisested-info spisested)
+          [:p.mt-5 "Tidligere tilsynsresultater:"]
+          [:div.flex.gap-5
+           (map mini-tilsynsresultat (take 4 besøk))]]
+         (siste-tilsynsresultat siste-besøk karakter)]
+        [:p.mt-5.text-xs "Mattilsynets smilefjestjeneste er nede på grunn av teknisk svikt, men vi jobber iherdig med å få på plass en erstatning. Nå har du snublet inn i vårt pågående arbeid. Kos med kaos!"]]]
+      [:div
+       [:div.max-w-screen-md.mx-auto.p-5
+        [:h2.text-2xl "Vurdering"]
+        [:p.my-2 (plakaten/oppsummer-smilefjeskarakter karakter)]]])))
 
 (defn render-page [ctx page]
   (case (:page/kind page)
