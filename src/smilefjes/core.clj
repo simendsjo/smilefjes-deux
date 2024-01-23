3fae2ac3db1075e2c8b7ac187f1b52fde7056dd7
Christian Johansen <christian.johansen@mattilsynet.no>
2024-01-23 09:33:14 +0100

    Klar for prod 🤠

--- src/smilefjes/core.clj
+++ src/smilefjes/core.clj
@@ -25,12 +25,11 @@
            :powerpack/source-dirs ["src" "dev"]
            :powerpack/resource-dirs ["resources"]
            :powerpack/port 5055
-           :powerpack/log-level :debug
            :powerpack/render-page #'pages/render-page
            :powerpack/on-started #'ingest/on-started
            :powerpack/get-context #'get-context}
-    ;; (= :build env)
-    ;; (assoc :site/base-url "https://smilefjes.mattilsynet.no")
+    (= :build env)
+    (assoc :site/base-url "https://smilefjes.mattilsynet.no")
 
     (= :dev env) ;; serve figwheel compiled js
     (assoc :powerpack/dev-assets-root-path "public")))
