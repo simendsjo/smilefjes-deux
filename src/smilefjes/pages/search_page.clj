56f6883993a85fae11e87a254c1942efb685debd
Christian Johansen <christian.johansen@mattilsynet.no>
2024-01-24 12:01:51 +0100

    Tilpass figur og knapp på forsiden

--- src/smilefjes/pages/search_page.clj
+++ src/smilefjes/pages/search_page.clj
@@ -1,7 +1,7 @@
 (ns smilefjes.pages.search-page
   (:require [clojure.java.io :as io]
             [datomic-type-extensions.api :as d]
-            [smilefjes.icons :as icons]
+            [fontawesome.icons :as icons]
             [smilefjes.layout :as layout]
             [smilefjes.link :as link]
             [smilefjes.search-index :as index]
@@ -45,19 +45,19 @@
       [:fieldset.mt-1
        {:class [:relative :w-full "min-w-36" :h-10]}
        [:div.replicant-root {:data-view "autocomplete"}]]]]
-    [:div.max-w-screen-lg.mx-auto.md:flex.px-8.py-4.justify-between.items-center
-     [:div.max-w-72.py-4
+    [:div.max-w-screen-lg.mx-auto.sm:flex.px-8.py-4.md:py-8.justify-around.items-center
+     [:div.max-w-72.py-4.md
       [:h2.text-lg.font-medium.mb-4 "Alle smilefjes på ett sted"]
       [:p.mb-4
        "Et smilefjes fra Mattilsynet viser hvordan vi vurderer viktige forhold
        som hygiene, rengjøring og vedlikehold på et spisested. Her finner du
        alle smilefjestilsyn i Norge siden 2016."]
-      [:a.flex.items-center.border.rounded.px-4.py-2.font-medium.border-granskog-800.border-2.text-granskog-800
+      [:a.inline-flex.items-center.border.rounded.px-4.py-2.font-medium.border-granskog-800.border-2.text-granskog-800
        {:href "https://www.mattilsynet.no/mat-og-drikke/forbrukere/smilefjesordningen"}
-       [:div.w-4.mr-2 icons/smilefjes]
-       "Les mer om smilefjes"]]
-     [:div.max-w-full.py-4.w-full
-      [:div.md:pl-16 (svg "/images/inspektør.svg")]]]))
+       "Les mer om smilefjes"
+       [:div.w-4.ml-2 (icons/render :fontawesome.solid/arrow-right)]]]
+     [:div.max-w-80.py-4.w-full
+      (svg "/images/inspektør.svg")]]))
 
 (defn render-result-page [ctx page]
   (layout/with-layout ctx page
