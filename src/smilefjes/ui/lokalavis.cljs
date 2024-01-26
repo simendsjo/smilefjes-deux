da7c92e85371c57d5086f1c4f3204961eb297084
Magnar Sveen <magnar.sveen@mattilsynet.no>
2024-01-26 09:21:06 +0100

    Hold riktig styring på hvilke tilsyn som er sett og ikke
    Det var naivt å tro at date-now kom til å fungere. Vi får jo inn tilsynene på
    etterskudd, ofte 1-2 dager etter. Vi må heller bruke seneste dato i datasettet.
    Dette er heller ikke feilfritt, fordi vi kan få inn flere tilsyn på samme dato -
    noen på tiden, noen på etterskudd - men dette er i hvert fall en bedre løsning.

--- src/smilefjes/ui/lokalavis.cljs
+++ src/smilefjes/ui/lokalavis.cljs
@@ -3,17 +3,18 @@
 
 (defn setup [kommunekode]
   (let [storage-key (str "kommune-" kommunekode)
-        date-now (subs (pr-str (js/Date.)) 7 17)
+        elements (js/document.querySelectorAll
+                  "[data-last_visit_date]")
+        newest-date (last (sort (map #(.getAttribute % "data-last_visit_date") elements)))
         [date-prev-visit date-pprev-visit] (storage/get-edn storage-key)
         comparison-date (when date-prev-visit
-                          (if (= date-now date-prev-visit)
+                          (if (= newest-date date-prev-visit)
                             date-pprev-visit
                             date-prev-visit))]
-    (when (not= date-now date-prev-visit)
-      (storage/set-edn storage-key [date-now date-prev-visit]))
+    (when (not= newest-date date-prev-visit)
+      (storage/set-edn storage-key [newest-date date-prev-visit]))
     (when comparison-date
       (doseq [report (filter #(< comparison-date
                                  (.getAttribute % "data-last_visit_date"))
-                             (js/document.querySelectorAll
-                              "[data-last_visit_date]"))]
+                             elements)]
         (.add (.-classList report) "ny-rapport")))))
